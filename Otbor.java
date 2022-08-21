ffab5154ea97f93560fa7d290e744cc27bdc3e25
Сергей Максенков <56511303+maksenkov@users.noreply.github.com>
2022-08-21 22:07:40 +0300

    код для opencsv

--- Otbor.java
+++ Otbor.java
@@ -1,6 +1,18 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Otbor {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        CSVReader reader = new CSVReader(new FileReader("f1_.csv"),',','"',1);
+        String[] nextLine;
+        while ((nextLine = reader.readNext()) != null) {
+            if (nextLine != null) {
+                //Verifying the read data here
+                System.out.println(Arrays.toString(nextLine));
 
+            }
+        }
     }
 }
